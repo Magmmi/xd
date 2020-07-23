8b6db6a7ee67def809c16b7a84d82ba71579a4d3
Magmmi <68044105+Magmmi@users.noreply.github.com>
2020-07-23 05:00:19 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/**   Downloaded NPM   **/
+const express = require("express");
+const cors = require("cors");
+
+//455
+/**   Variables   **/
+const app = express();
+app.use(cors());
+
+/**   Local NPM   **/
+require("./src/database/mongoose");
+const accountRouter = require("./src/routers/account");
+const productRouter = require("./src/routers/product");
+
+/**   express uses   **/
+app.use(express.json());
+app.use(accountRouter);
+app.use(productRouter);
+
+/**   express functions   **/
+app.listen(5000, () => {
+  console.log("Server up on 5000");
+});
